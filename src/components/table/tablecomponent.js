f301b0067228c049b67be6aa09b0099210b9a663
dakaniramesh@21 <dakaniramesh21@gmail.com>
2024-01-10 12:08:01 +0530

    pushing

---
+++ src/components/table/tablecomponent.js
@@ -0,0 +1,34 @@
+function TableComponent(){
+    return(
+        <div>
+           <table>
+        <tr>
+          <th>Id</th>
+          <th>Course</th>
+          <th>College</th>
+          <th>Year</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Civil</td>
+          <td>Mrits</td>
+          <td>2023</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Mpc</td>
+          <td>TS Model College</td>
+          <td>2019</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Ssc</td>
+          <td>ZP High School</td>
+          <td>2017</td>
+        </tr>
+
+      </table>
+        </div>
+    )
+}
+export default TableComponent
